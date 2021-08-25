4da31d1c7826eeb0d2aa8234dedce619c0470c65
Vladyslav Zuiev <vladyslav.zuiev@bshg.com>
2021-08-26 00:34:05 +0200

    menage cats

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import { Component } from 'react';
 import Head from 'next/head';
-import { attributes, html } from '../content/home.md';
+import { attributes } from '../content/home.md';
 
 export default class Home extends Component {
   render() {
     const { title, cats } = attributes;
+
+    console.log(attributes);
 
     return (
       <>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
-        <h1>{attributes.title}</h1>
         <article>
           <h1>{title}</h1>
-          {/* <HomeContent />  */}
           <ul>
             {cats.map((cat, k) => (
               <li key={k}>
@@ -24,7 +24,6 @@ export default class Home extends Component {
             ))}
           </ul>
         </article>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
         <style jsx>{`
           h1,
           div {
